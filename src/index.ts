a1f5234e53b29c453e37bd8c40c392654d44b19d
Andrea Bondanini <andrea.bondanini.ext@abilio.com>
2024-01-18 17:05:30 +0100

    Render image from S3

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,35 @@
-import PDFDocument from "pdfkit";
 import * as fs from "fs";
+import PDFDocument from "pdfkit";
+import * as announcement from "./source.json";
+import axios from "axios";
 
-const doc = new PDFDocument({
-  size: "A4",
-  font: "Courier",
-  layout: "landscape",
-});
+async function generate() {
+  const doc = new PDFDocument({
+    size: "A4",
+    font: "Courier",
+    layout: "landscape",
+  });
 
-doc.pipe(fs.createWriteStream("./file.pdf")); // write to PDF
+  const logo = await fetchImage(
+    announcement.content.M.lot.M.images.L[0]?.M.urls.M.large.S
+  );
 
-// add stuff to PDF here using methods described below...
-doc.addPage({ layout: "landscape" });
+  doc.image(logo, 100, 200, {scale: 0.25});
 
-// finalize the PDF and end the stream
-doc.end();
+  doc.pipe(fs.createWriteStream("./file.pdf")); // write to PDF
+
+  // add stuff to PDF here using methods described below...
+  doc.addPage({ layout: "landscape" });
+
+  // finalize the PDF and end the stream
+  doc.end();
+}
+
+async function fetchImage(src: string) {
+  const image = await axios.get(src, {
+    responseType: "arraybuffer",
+  });
+  return image.data;
+}
+
+generate();
