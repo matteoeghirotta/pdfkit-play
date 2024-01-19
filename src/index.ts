a16d4eed002fa06587e44b265c74d3f1635effa0
Andrea Bondanini <andrea.bondanini.ext@abilio.com>
2024-01-19 10:28:31 +0100

    Home page example with pdfkit

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,62 @@
-// import * as fs from "fs";
-// import PDFDocument, { circle, options } from "pdfkit";
-// import * as announcement from "./source.json";
-// import axios from "axios";
+import * as fs from "fs";
+import PDFDocument, { opacity } from "pdfkit";
+import * as announcement from "./source.json";
+import axios from "axios";
 
-// async function generate() {
-//   const doc = new PDFDocument({
-//     size: "A4",
-//     font: "Courier",
-//     layout: "landscape",
-//   });
+async function generate() {
+  const doc = new PDFDocument({
+    size: "A4",
+    font: "Courier",
+    layout: "landscape",
+  });
 
-//   const image = await fetchImage(
-//     announcement.content.M.lot.M.images.L[0]?.M.urls.M.large.S
-//   );
+  const image = await fetchImage(
+    announcement.content.M.lot.M.images.L[0]?.M.urls.M.large.S
+  );
 
-//   // ===== HEADER
+  const title = announcement.title.S;
 
-//   // Drawing horizontal line
-//   doc.moveTo(20, 20).lineTo(820, 20).lineWidth(2).fillAndStroke("#006FDF");
+  // ===== HEADER
 
-//   // TODO: put logo & QR Code
+  // Drawing horizontal line
+  doc.moveTo(20, 20).lineTo(820, 20).lineWidth(2).fillAndStroke("#006FDF");
 
-//   // Drawing background image
-//   doc.image(image, 20, 80, {width: 800, height: 450});
+  // TODO: put logo & QR Code
 
-//   doc.pipe(fs.createWriteStream("./file.pdf")); // write to PDF
+  // Add an image, constrain it to a given size, and center it vertically and horizontally
+  doc.roundedRect(20, 80, 800, 450, 15).clip().image(image, 20, 80, {
+    width: 800,
+    height: 450,
+  });
 
-//   // add stuff to PDF here using methods described below...
-//   doc.addPage({ layout: "landscape" });
+  doc
+    .roundedRect(30, 100, 400, 150, 15)
+    .fillOpacity(0.8)
+    .fillAndStroke("#ebedf0")
+    .restore();
 
-//   // finalize the PDF and end the stream
-//   doc.end();
-// }
+  doc
+    .fillColor("black")
+    .font("Helvetica-Bold")
+    .fontSize(20)
+    .text(title, 45, 115, {
+      width: 380,
+    });
 
-// async function fetchImage(src: string) {
-//   const image = await axios.get(src, {
-//     responseType: "arraybuffer",
-//   });
-//   return image.data;
-// }
+  doc.pipe(fs.createWriteStream("./file.pdf")); // write to PDF
 
-// generate();
+  // add stuff to PDF here using methods described below...
+  doc.addPage({ layout: "landscape" });
+
+  // finalize the PDF and end the stream
+  doc.end();
+}
+
+async function fetchImage(src: string) {
+  const image = await axios.get(src, {
+    responseType: "arraybuffer",
+  });
+  return image.data;
+}
+
+generate();
